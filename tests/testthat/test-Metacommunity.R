35e97f05bded85c60b0ed3fe0815aec349372b99
taddallas <tdallas@uga.edu>
2015-12-30 20:50:25 -0500

    sweeping changes to standardize arguments, and switch over to roxygenize documentation

--- tests/testthat/test-Metacommunity.R
+++ tests/testthat/test-Metacommunity.R
@@ -1,15 +1,9 @@
 context("Metacommunity")
 
 test_that("Metacommunity is equal to sum of parts", {
-	set.seed(100); 
-  test=matrix(rbinom(100,1,0.5), ncol=10)
-  mc.test=Metacommunity(test, sims=100, method='swap')
-  #coh.test=Coherence(test, sims=100, method='swap')
-  #tur.test=Turnover(test, sims=100, method='swap')
-  bc.test=BoundaryClump(test)
-  
-	#expect_equal(mc.test$Coherence$EmbAbs, coh.test$EmbAbs)
-	#expect_equal(mc.test$Turnover$replacements, tur.test$Turnover)
-  expect_equal(mc.test$Boundary$index, bc.test$index)
+	set.seed(100);
+	test <- matrix(rbinom(100,1,0.5), ncol=10)
+	mc.test <- Metacommunity(test, sims=100, method='swap')
+	bc.test <- BoundaryClump(test)
+	expect_equal(mc.test$Boundary['index'], bc.test['index'])
 })
-	
