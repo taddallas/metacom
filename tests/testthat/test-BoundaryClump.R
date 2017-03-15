8ddf90e7e2857ab2af5d5578d3c0ae182ecff507
taddallas <tdallas@uga.edu>
2017-03-15 11:01:18 -0700

    BoundaryClump test throwing error but works on my machine?

--- tests/testthat/test-BoundaryClump.R
+++ tests/testthat/test-BoundaryClump.R
@@ -1,9 +1,9 @@
 context("BoundaryClump")
 
 test_that("Metacommunity and BoundaryClump agree", {
-	set.seed(100);
+	set.seed(1000);
 	test <- matrix(rbinom(100,1,0.5), ncol=10)
-	mc.test <- Metacommunity(test, sims=100, method='swap')
+	mc.test <- Metacommunity(test, sims=10, method='swap')
 	bc.test <- BoundaryClump(test)
 	expect_equal(as.numeric(mc.test$Boundary[1]), 
     as.numeric(bc.test[1]))
